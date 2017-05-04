e69fec8030976978540e8f708be899e6e3cdf924
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 16:39:12 +0530

    Added InfiniteForLoop.java under control_flow/iteration_statements

---
+++ in/codesource/programs/java/control_flow/iteration_statements/InfiniteForLoop.java
@@ -0,0 +1,9 @@
+package in.codesource.programs.java.control_flow.iteration_statements;
+
+public class InfiniteForLoop {  
+	public static void main(String[] args) {  
+		for(;;) {  
+			System.out.println("for loop is infinite");  
+		}  
+	}  
+}
