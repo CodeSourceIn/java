b98611a5da2d21f36f5bfa16914abb1bfe8737ec
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 17:01:52 +0530

    Added SimpleIf.java control_flow/selection_statements

---
+++ in/codesource/programs/java/control_flow/selection_statements/SimpleIf.java
@@ -0,0 +1,11 @@
+package in.codesource.programs.java.control_flow.selection_statements;
+
+public class SimpleIf {
+	public static void main(String[] args) {
+		int a=5;
+		int b=8;
+		if(a!=b) {
+			System.out.println("A is not equal to B");
+		}
+	}
+}
