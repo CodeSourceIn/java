e27afd44a74cf34d8457fdbcb240506bc7be6954
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 17:02:12 +0530

    Added SimpleIfElse.java control_flow/selection_statements

---
+++ in/codesource/programs/java/control_flow/selection_statements/SimpleIfElse.java
@@ -0,0 +1,12 @@
+package in.codesource.programs.java.control_flow.selection_statements;
+
+public class SimpleIfElse {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		if (a>b)
+			System.out.println("A is bigger then B");
+		else 
+			System.out.println("B is bigger than A");
+	}
+}
