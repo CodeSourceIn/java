017c66209d51561a0e796117e271472308a4caac
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 16:38:49 +0530

    Added InfiniteDoWhileLoop.java under control_flow/iteration_statements

---
+++ in/codesource/programs/java/control_flow/iteration_statements/InfiniteDoWhileLoop.java
@@ -0,0 +1,9 @@
+package in.codesource.programs.java.control_flow.iteration_statements;
+
+public class InfiniteDoWhileLoop {  
+	public static void main(String[] args) {  
+		do {  
+			System.out.println("do while loop is infinite");  
+		}while(true);
+	}  
+}
