9f28913a17dee58556ccae718d3c1b50294857a3
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 16:12:00 +0530

    Added SimpleDoWhileLoop.java under control_flow/iteration_statements

--- in/codesource/programs/java/control_flow/iteration_statements/SimpleDoWhileLoop.java
+++ in/codesource/programs/java/control_flow/iteration_statements/SimpleDoWhileLoop.java
@@ -3,19 +3,19 @@ package in.codesource.programs.java.control_flow.iteration_statements;
 public class SimpleDoWhileLoop {
 	public static void main(String args[]) {
 		int i=1;
-		System.out.println("Simple do while loop - increment by 2");
-		System.out.println("-------------------------------------");
+		System.out.println("Simple do while loop - increment");
+		System.out.println("--------------------------------");
 		do {
 			System.out.print(i + ", ");
-			i+=2;
+			i++;
 		}while(i<=10);
 		i=10;
 		System.out.println("\n");
-		System.out.println("Simple do while loop - decrement by 2");
-		System.out.println("-------------------------------------");
+		System.out.println("Simple do while loop - decrement");
+		System.out.println("--------------------------------");
 		do {
 			System.out.print(i + ", ");
-			i-=2;
+			i--;
 		}while(i>=1);
 	}
 }
