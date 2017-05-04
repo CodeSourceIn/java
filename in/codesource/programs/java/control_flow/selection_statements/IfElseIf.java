be6015afa6148a154578c2e69c33b1a0c5a3d018
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 17:00:22 +0530

    Added IfElseIf.java control_flow/selection_statements

---
+++ in/codesource/programs/java/control_flow/selection_statements/IfElseIf.java
@@ -0,0 +1,14 @@
+package in.codesource.programs.java.control_flow.selection_statements;
+
+public class IfElseIf {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		if(a==b)
+			System.out.println("A is equal to B");
+		else if (a>b)
+			System.out.println("A is bigger than B");
+		else 
+			System.out.println("B is bigger than A");
+	}
+}
