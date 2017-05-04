8dc58b0d7cc20d0fb16d9250b5d57915fa67ed95
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-04 16:40:09 +0530

    Added SimpleForLoop.java under control_flow/iteration_statements

--- in/codesource/programs/java/control_flow/iteration_statements/SimpleForLoop.java
+++ in/codesource/programs/java/control_flow/iteration_statements/SimpleForLoop.java
@@ -1,5 +1,17 @@
 package in.codesource.programs.java.control_flow.iteration_statements;
 
 public class SimpleForLoop {
-
+	public static void main(String[] args) {
+		System.out.println("Simple for loop - increment");
+		System.out.println("---------------------------");
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(i + ", ");
+		}
+		System.out.println("\n");
+		System.out.println("Simple for loop - decrement");
+		System.out.println("---------------------------");
+		for(int i = 10; i >= 1; i--) {
+			System.out.print(i + ", ");
+		}
+	}
 }
