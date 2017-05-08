7727f0aaa2c4df5cb209d133b8564c4aa8e55b28
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-08 11:27:23 +0530

    Added SimpleArray.java under declaration/arrays

---
+++ in/codesource/programs/java/declarations/arrays/SimpleArray.java
@@ -0,0 +1,10 @@
+package in.codesource.programs.java.declarations.arrays;
+
+public class SimpleArray {
+	public static void main(String[] args) {
+		System.out.println("Simple Array Usage");
+		int a[]={1,2,3};
+		for(int i=0;i<3;i++)
+			System.out.println("a[" + i + "]=" + a[i]);
+	}
+}
