0844c1012db7e757f4ba537e05cf67b7111b733e
b <barani@dhrti.net>
2017-05-08 15:31:42 +0530

    Minor changes to Array Declarations program

--- in/codesource/programs/java/declarations/arrays/ArrayDeclarations.java
+++ in/codesource/programs/java/declarations/arrays/ArrayDeclarations.java
@@ -2,35 +2,38 @@ package in.codesource.programs.java.declarations.arrays;
 
 public class ArrayDeclarations {
 	public static void main(String[] args) {
-		float a[]=new float[5];
+		float a[];
+		a = new float[5];
 		a[0]=10.0f;
 		a[1]=20.0f;  
 		a[3]=30.0f;  
 		a[4]=40.0f;   
 		System.out.println();
-		System.out.println("Array Declaration- float");  
+		System.out.println("Array Declaration - float");  
 		System.out.println("------------------------");   
 		for(int i=0;i<a.length;i++)
 			System.out.println("a[" + i + "] = " + a[i]); 
 
-		char b[]=new char[5];
+		char b[];
+		b = new char[5];
 		b[0]='A';
 		b[1]='B';  
 		b[2]='C';  
 		b[3]='D';   
 		System.out.println();
-		System.out.println("Array Declaration- char");  
+		System.out.println("Array Declaration - char");  
 		System.out.println("-----------------------");   
 		for(int i=0;i<b.length;i++)
 			System.out.println("b[" + i + "] = " + b[i]); 
 
-		String[] c=new String[5];
+		String[] c;
+		c = new String[5];
 		c[0]="C#";
 		c[2]="Java";  
 		c[3]="C++";  
 		c[4]="Oracle";   
 		System.out.println();
-		System.out.println("Array Declaration- string");  
+		System.out.println("Array Declaration - string");  
 		System.out.println("-------------------------");   
 		for(int i=0;i<c.length;i++)
 			System.out.println("c[" + i + "] = " + c[i]); 
