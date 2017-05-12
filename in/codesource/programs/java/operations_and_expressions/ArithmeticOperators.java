49a80f022878a837b104737793dfeae2286c6a0b
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-12 16:13:36 +0530

    Added ArithmeticOperators.java under operations_and_expressions

---
+++ in/codesource/programs/java/operations_and_expressions/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package in.codesource.programs.java.operations_and_expressions;
+
+public class ArithmeticOperators {
+    public static void main(String [] args) {
+        int A = 7, B = 2;
+        System.out.println("A value: " + A);
+        System.out.println("B value: " + B);
+        System.out.println("A+B value:" + (A + B));
+        System.out.println("A-B value:" + (A - B));
+        System.out.println("A*B value:" + (A * B));
+        System.out.println("A/B value:" + (A / B));
+        System.out.println("A%B value:" + (A % B));
+    }
+}
