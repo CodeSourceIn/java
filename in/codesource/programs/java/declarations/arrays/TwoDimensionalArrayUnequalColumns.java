5b0463cae16fef0aa71a3f9f28520f285739c73e
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-12 15:31:04 +0530

    Added TwoDimensionalArrayUnequalColumns.java under declarations/arrays

--- in/codesource/programs/java/declarations/arrays/TwoDimensionalArrayUnequalColumns.java
+++ in/codesource/programs/java/declarations/arrays/TwoDimensionalArrayUnequalColumns.java
@@ -3,12 +3,14 @@ package in.codesource.programs.java.declarations.arrays;
 public class TwoDimensionalArrayUnequalColumns {
     public static void main(String [] args) {
         System.out.println("Two Dimensional Array Unequal Columns");
-        int a[][] = new int [3] [];
-        a[0] = new int [2];
-        a[1] = new int [2];
-        a[2] = new int [2];
-        for ( int i = 0; i < 3; i++ ) {
-            for ( int j = 0; j < 2; j++ ) {
+        int a[][] = new int [5] [];
+        a[0] = new int [3];
+        a[1] = new int [5];
+        a[2] = new int [4];
+        a[3] = new int [6];
+        a[4] = new int [2];
+        for ( int i = 0; i < a.length; i++ ) {
+            for ( int j = 0; j < a[i].length; j++ ) {
                 System.out.print("a[" + i + "][" + j + "] = " + a[i][j] + "\t");
             }
             System.out.println("");
