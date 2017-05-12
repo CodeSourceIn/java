ef1c4afa0eb1ea4d4c891b18fec5b4c2507badf7
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-12 16:49:15 +0530

    Added TernaryConditionalOperator.java under operations_and_expressions/conditional_operators

---
+++ in/codesource/programs/java/operations_and_expressions/TernaryConditionalOperator.java
@@ -0,0 +1,18 @@
+package in.codesource.programs.java.operations_and_expressions;
+
+public class TernaryConditionalOperator {
+    public static void main(String [] args) {
+        int a, b;
+        a = 10;
+        System.out.println("Ternary Operator's condition true");
+        System.out.println("---------------------------------");
+        b = (a == 10) ? 0 : 1;
+        System.out.println("Value of b is : " + b);
+        a++;
+        System.out.println("");
+        System.out.println("Ternary Operator's condition false");
+        System.out.println("----------------------------------");
+        b = (a == 0) ? 0 : 1;
+        System.out.println("Value of b is : " + b);
+    }
+}
