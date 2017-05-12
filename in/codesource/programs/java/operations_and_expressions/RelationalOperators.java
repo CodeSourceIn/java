0cd51906d98dce1d423a6b300a951562557e78c8
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-12 16:31:12 +0530

    Added RelationalOperators.java under operations_and_expressions

---
+++ in/codesource/programs/java/operations_and_expressions/RelationalOperators.java
@@ -0,0 +1,18 @@
+package in.codesource.programs.java.operations_and_expressions;
+
+public class RelationalOperators {
+    public static void main(String [] args) {
+        int a = 5;
+        int b = 5;
+        int c = 8;
+        System.out.println(a + " is equal to " + b + " : " + (a == b));
+        System.out.println(a + " is equal to " + c + " : " + (a == c));
+        System.out.println(a + " is less than " + c + " : " + (a < c));
+        System.out.println(a + " is not equal to " + b + " : " + (a != b));
+        System.out.println(a + " is not equal to " + c + " : " + (a != c));
+        System.out.println(a + " is greater than " + c + " : " + (a > c));
+        System.out.println(a + " is less than or equal to " + c + " : " + (a <= c));
+        System.out.println(a + " is greater than or equal to " + b + " : " + (a >= b));
+        System.out.println(a + " is greater than or equal to " + c + " : " + (a >= c));
+    }
+}
