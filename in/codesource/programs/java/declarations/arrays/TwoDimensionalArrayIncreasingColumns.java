4b275b14dbb63fe484f53caa21b6e4f746d32d4f
b <barani@dhrti.net>
2017-05-19 11:35:24 +0530

    Minor Changes to Two Dimensional array programs

--- in/codesource/programs/java/declarations/arrays/TwoDimensionalArrayIncreasingColumns.java
+++ in/codesource/programs/java/declarations/arrays/TwoDimensionalArrayIncreasingColumns.java
@@ -4,14 +4,14 @@ public class TwoDimensionalArrayIncreasingColumns {
     public static void main(String[] args) {
         System.out.println("Two Dimensional Array with Increasing Column Size");
         int a[][] = new int[5][];
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++)
             a[i] = new int[i + 1];
-        }
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++)
+                a[i][j] = i + j + 1;
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0, k = 1; j < a[i].length; j++, k++) {
-                a[i][j] = k;
+            for (int j = 0; j < a[i].length; j++)
                 System.out.print(a[i][j] + " ");
-            }
             System.out.println();
         }
     }
