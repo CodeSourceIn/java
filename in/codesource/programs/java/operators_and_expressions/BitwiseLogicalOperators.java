e7ed6886808a38cf522a01c15ce6559b4fa2b0a5
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-31 11:06:52 +0530

    Added BitwiseLogicalOperators.java under operations_and_expressions

--- in/codesource/programs/java/operators_and_expressions/BitwiseLogicalOperators.java
+++ in/codesource/programs/java/operators_and_expressions/BitwiseLogicalOperators.java
@@ -1,7 +1,7 @@
 package in.codesource.programs.java.operators_and_expressions;
 
 public class BitwiseLogicalOperators {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         int a = 37; /* 37 = 0010 0101 */
         int b = 13; /* 13 = 0000 1101 */
         int c = 0;
@@ -17,17 +17,5 @@ public class BitwiseLogicalOperators {
 
         c = ~a; /*-38 = 1101 1010 */
         System.out.println("~" + a + " = " + c);
-<<<<<<< HEAD:in/codesource/programs/java/operators_and_expressions/BinaryandShiftOperator.java
-
-        c = a << 2; /* 148 = 1001 0100 */
-        System.out.println(a + " << 2 = " + c);
-
-        c = 22 >> 2; /* 9 = 0000 1001 */
-        System.out.println(a + " >> 2  = " + c);
-
-        c = 22 >>> 2; /* 9 = 0000 1001 */
-        System.out.println(a + " >>> 2 = " + c);
-=======
->>>>>>> 7d70388413d874b55078a23b1eb5b35a5d0b0f1b:in/codesource/programs/java/operators_and_expressions/BitwiseLogicalOperators.java
     }
 }
