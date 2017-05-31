60c2f7ddf11934fcfa40e3687f9d83db45d0e071
Aravinthraj <aravinthrajmsp@outlook.com>
2017-05-31 13:54:49 +0530

    Added UnaryOperators.java under operations_and_expressions

--- in/codesource/programs/java/operators_and_expressions/UnaryOperators.java
+++ in/codesource/programs/java/operators_and_expressions/UnaryOperators.java
@@ -3,6 +3,7 @@ package in.codesource.programs.java.operators_and_expressions;
 public class UnaryOperators {
     public static void main(String [] args) {
         int i = 5;
+        boolean j = true;
         System.out.println("Before i++ value:" + i);
         System.out.println("During i++ value:" + (i++));
         System.out.println("After i++ value:" + i);
@@ -20,13 +21,12 @@ public class UnaryOperators {
         System.out.println("After --i value:" + i);
 
         System.out.println("\nBefore +i value:" + i);
-        System.out.println("During +i value:" + (+i));
+        System.out.println("After +i value:" + (+i));
 
         System.out.println("\nBefore -i value:" + i);
-        System.out.println("During -i value:" + (-i));
+        System.out.println("After -i value:" + (-i));
 
-        System.out.println("\nBefore ~i value:" + i);
-        System.out.println("During ~i value:" + ~i);
-        System.out.println("After ~i value:" + i);
+        System.out.println("\nBefore !i value:" + j);
+        System.out.println("After !i value:" + (!j));
     }
 }
