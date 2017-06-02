8d58a105caade8f91917d59acb84d2af71ab4f5f
b <barani@dhrti.net>
2017-06-02 17:43:01 +0530

    Done some changes to unary operators program

--- in/codesource/programs/java/operators_and_expressions/UnaryOperators.java
+++ in/codesource/programs/java/operators_and_expressions/UnaryOperators.java
@@ -1,9 +1,9 @@
 package in.codesource.programs.java.operators_and_expressions;
 
 public class UnaryOperators {
-    public static void main(String [] args) {
-        int i = 5;
-        boolean j = true;
+    public static void main(String[] args) {
+        int i = 5, j = -5;
+        boolean k = true;
         System.out.println("Before i++ value:" + i);
         System.out.println("During i++ value:" + (i++));
         System.out.println("After i++ value:" + i);
@@ -20,13 +20,21 @@ public class UnaryOperators {
         System.out.println("During --i value:" + (--i));
         System.out.println("After --i value:" + i);
 
-        System.out.println("\nBefore +i value:" + i);
-        System.out.println("After +i value:" + (+i));
+        i = 5;
+        j = -10;
+        System.out.println("\n i value:" + i);
+        System.out.println(" +i value:" + (+i));
 
-        System.out.println("\nBefore -i value:" + i);
-        System.out.println("After -i value:" + (-i));
+        System.out.println("\n j value:" + j);
+        System.out.println(" +j value:" + (+j));
 
-        System.out.println("\nBefore !i value:" + j);
-        System.out.println("After !i value:" + (!j));
+        System.out.println("\n i value:" + i);
+        System.out.println(" -i value:" + (-i));
+
+        System.out.println("\n j value:" + j);
+        System.out.println(" -j value:" + (-j));
+
+        System.out.println("\n k value:" + k);
+        System.out.println(" !k value:" + (!k));
     }
 }
