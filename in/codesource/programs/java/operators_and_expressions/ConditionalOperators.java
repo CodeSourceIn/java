da25a4b2fc344d109ea7d3fa131e65cd0f1925da
Aravinthraj <aravinthrajmsp@outlook.com>
2017-07-17 11:30:43 +0530

    Added ConditionalOperators.java under operators_and_expressions

---
+++ in/codesource/programs/java/operators_and_expressions/ConditionalOperators.java
@@ -0,0 +1,25 @@
+package in.codesource.programs.java.operators_and_expressions;
+
+public class ConditionalOperators {
+    public static void main(String [] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        boolean d = true;
+
+        System.out.println("'&&' operator");
+        System.out.println("-------------");
+        System.out.println("true && true = " + (true && true));
+        System.out.println("true && false = " + (true && false));
+        System.out.println("false && true = " + (false && true));
+        System.out.println("\n'||' operator");
+        System.out.println("-------------");
+        System.out.println("true || true = " + (true || true));
+        System.out.println("true || false = " + (true || false));
+        System.out.println("false || true = " + (false || true));
+        System.out.println();
+        System.out.println("a>=b && a>=c :" + (a >= b && a >= c));
+        System.out.println("a>=b || b>=c :" + (a >= b || b >= c));
+        System.out.println("c>a && d :" + (c > a && d));
+    }
+}
